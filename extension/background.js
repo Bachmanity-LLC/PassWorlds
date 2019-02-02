4194d97d168acb7caaa50bec7490ac4dac25faf1
shivam1708 <shivam.pawase@somaiya.edu>
2019-02-02 12:43:03 +0530

    Basic extension

---
+++ extension/background.js
@@ -0,0 +1,15 @@
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.set({color: '#3aa757'}, function() {
+      console.log("The color is green.");
+    });
+
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([{
+          conditions: [new chrome.declarativeContent.PageStateMatcher({
+            //pageUrl: { urlMatches: '()\.com' },
+          })
+          ],
+              actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
+});
